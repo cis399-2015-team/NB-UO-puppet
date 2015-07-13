05405c3573783b4e43422c8db08f5ac44761538e
root <root@ip-10-0-5-217.us-west-2.compute.internal>
2015-07-13 00:18:58 +0000

    testing very dangerous

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,6 +6,7 @@ node ip-10-0-5-217{
 }
 #Tianhao Qian
 node ip-10-0-5-179{
+        include sshd
 	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master",
 	    user    => root,
