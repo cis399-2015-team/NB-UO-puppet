82c8a2305795138f2aadabf2dcff209ca9fef3b5
root <root@ip-10-0-5-179.us-west-2.compute.internal>
2015-07-25 22:53:23 +0000

    update

--- modules/leili_html/manifests/init.pp
+++ modules/leili_html/manifests/init.pp
@@ -3,6 +3,6 @@ class leili_html {
 		mode => 644,
 		owner => "root",
 		group => "root",
-		source => "/etc/puppet/modules/heqianli_html/files/index.html"
+		source => "/etc/puppet/modules/leili_html/files/index.html"
 	}
 }
--- modules/tianhaoqian_html/manifests/init.pp
+++ modules/tianhaoqian_html/manifests/init.pp
@@ -3,6 +3,6 @@ class tianhaoqian_html {
 		mode => 644,
 		owner => "root",
 		group => "root",
-		source => "/etc/puppet/modules/heqianli_html/files/index.html"
+		source => "/etc/puppet/modules/tianhaoqian_html/files/index.html"
 	}
 }
